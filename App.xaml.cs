01655eee810e48b1cc37214213802dbebe86326a
thuso kharibe <thusok@runninghill.co.za>
2024-08-13 16:28:34 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace PagesDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var navPage = new NavigationPage(new MainPage());
+
+            navPage.BarBackground = Colors.Chocolate;
+            navPage.BarTextColor = Colors.Aqua;
+
+            MainPage = new TabbedPageDemo();
+        }
+    }
+}
